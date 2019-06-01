7e6d231059db7a88accd064b9bd4b634f5262dc9
Antonio Sviridov <ant.svirido@gmail.com>
2019-06-01 22:53:53 +0300

    add base controller

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private Vector3 _forceHit;
+    [SerializeField] private Vector3 _direction;
+    [SerializeField] private float _maxVelocityY;
+    
+    private Rigidbody _rb;
+    private bool _isHit;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+//        if (_isHit)
+//        {
+//            _rb.AddForce(_direction * _forceHit, ForceMode.Force);
+//        }
+    }
+
+    private void Update()
+    {
+        Debug.Log(_rb.velocity);
+    }
+
+    public void addImpulse()
+    {
+        _rb.velocity = Vector3.zero;
+        Debug.Log(_rb.velocity);
+        _rb.AddForce(_direction.x * _forceHit.x, _direction.y * _forceHit.y, _direction.z * _forceHit.z, ForceMode.Impulse);
+        _isHit = true;
+        Debug.Log(_rb.velocity);
+    }
+}
---
+++ Assets/Scripts/RotateController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class RotateController : MonoBehaviour
+{
+    [SerializeField]private float _step;
+    [SerializeField]private float _waitCoroutine;
+    [SerializeField] private int _maxAmountRotate;
+    [SerializeField] private int _currentAmountRotate;
+    private float oldTime;
+    
+    public static RotateController rt;
+    private Rigidbody _rb;
+    
+    
+    void Awake()
+    {
+        if (rt == null)
+        {
+            rt = new RotateController();
+        }
+    }
+
+    void Start()
+    {;
+        _rb = GetComponent<Rigidbody>();
+        oldTime = Time.time;
+    }
+
+    void Update()
+    {
+        Touch();
+    }
+
+    void FixedUpdate()
+    {
+
+        if (_currentAmountRotate != _maxAmountRotate && Time.time - oldTime > _waitCoroutine)
+        {
+            _rb.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y,
+                transform.rotation.eulerAngles.z + _step);
+            oldTime = Time.time;
+            _currentAmountRotate += 1;
+        }
+    }
+
+    private void Touch()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _currentAmountRotate = 0;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == 9 && IsRotate())
+        {
+            other.gameObject.GetComponent<BallController>().addImpulse();
+        }
+    }
+
+    public bool IsRotate()
+    {
+        return _currentAmountRotate != 36;
+    }
+}
